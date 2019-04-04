42a26f586684a41ef163a732123594863fa16442
Svetlozar <stormcrafterbg@gmail.com>
2019-04-04 19:52:00 +0300

    TaskManagerSimpleApp

---
+++ TechModule/WebBasics/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TeisterMask.Data;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var allTasks = db.Tasks.ToList();
+                return View(allTasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string status)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(status))
+            {
+                return RedirectToAction("Index");
+            }
+            Task newTask = new Task()
+            {
+                Title = title,
+                Status = status
+            };
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Add(newTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(x => x.Id == id);
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(taskToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(x => x.Id == task.Id);
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                taskToEdit.Title = task.Title;
+                taskToEdit.Status = task.Status;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToDelete = db.Tasks.FirstOrDefault(x => x.Id == id);
+                if (taskToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(taskToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToDelete = db.Tasks.FirstOrDefault(x => x.Id == task.Id);
+                if (taskToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                db.Tasks.Remove(taskToDelete);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

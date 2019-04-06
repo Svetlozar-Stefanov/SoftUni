8ff48ac3b8d67f245563f78172493a8f71d903ff
Svetlozar <stormcrafterbg@gmail.com>
2019-04-06 19:16:09 +0300

    TodaysDemoExam

---
+++ TechModule/DemoExam/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            string[] wordsAndDescriptions = Console.ReadLine().Split(" | ");
+
+            foreach (var pair in wordsAndDescriptions)
+            {
+                string[] pairInfo = pair.Split(": ");
+                string word = pairInfo[0];
+                string description = pairInfo[1];
+
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary.Add(word, new List<string>());
+                }
+                dictionary[word].Add(description);
+            }
+
+            string[] wordsToOutput = Console.ReadLine().Split(" | ");
+            foreach (var word in wordsToOutput)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    Console.WriteLine($"{word}");
+                    foreach (var description in dictionary[word].OrderByDescending(x => x.Length))
+                    {
+                        Console.WriteLine($" -{description}");
+                    }
+                }
+            }
+
+            string endCommand = Console.ReadLine();
+            if (endCommand == "End")
+            {
+                return;
+            }
+            if (endCommand == "List")
+            {
+                foreach (var word in dictionary.OrderBy(x =>  x.Key))
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
---
+++ TechModule/DemoExam/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] keys = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string firstSubstring = keys[0];
+            string secondSubstring = keys[1];
+
+            Regex regex = new Regex(@"^[d-z{}|#]+$");
+
+            if (regex.IsMatch(input))
+            {
+                StringBuilder decryption = new StringBuilder();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char currentLetter = input[i];
+                    decryption.Append((char)(currentLetter - 3));
+                }
+
+                string textToEdit = decryption.ToString();
+                textToEdit = textToEdit.Replace(firstSubstring, secondSubstring);
+                Console.WriteLine(textToEdit);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}

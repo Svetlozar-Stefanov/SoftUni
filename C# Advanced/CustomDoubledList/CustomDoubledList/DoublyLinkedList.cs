80882c4525f88c0ac3a1f6e9cbdc3260ac95d7eb
BountyHunter <stormcrafterbg@gmail.com>
2019-06-13 19:08:14 +0300

    doubled list

---
+++ C# Advanced/CustomDoubledList/CustomDoubledList/DoublyLinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoubledList
+{
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+        }
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get;private set; }
+
+        public void AddFirst(int value)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(value);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(value);
+                newHead.NextNode = head;
+                head.PreviousNode = newHead;
+                head = newHead;
+            }
+            Count++;
+        }
+
+        public void AddLast(int value)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(value);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(value);
+                newTail.PreviousNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+            Count++;
+        }
+        public int RemoveFirst()
+        {
+            CheckIfEmpty();
+            int element = head.Value;
+            head = head.NextNode;
+            if (head != null)
+            {
+                head.PreviousNode = null;
+            }
+            else
+            {
+                tail = null;
+            }
+            Count--;
+            return element;
+        }
+        public int RemoveLast()
+        {
+            CheckIfEmpty();
+            int element = tail.Value;
+
+            tail = tail.PreviousNode;
+            if (tail != null)
+            {
+                tail.NextNode = null;
+            }
+            else
+            {
+                head = null;
+            }
+            Count--;
+            return element;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentHead = head;
+            while (currentHead != null)
+            {
+                action(currentHead.Value);
+                currentHead = currentHead.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] arr = new int[Count];
+            int counter = 0;
+            var cuurentNode = head;
+            while (cuurentNode != null)
+            {
+                arr[counter] = cuurentNode.Value;
+                cuurentNode = cuurentNode.NextNode;
+                counter++;
+            }
+            return arr;
+        }
+        private void CheckIfEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ C# Advanced/CustomDoubledList/CustomDoubledList/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomDoubledList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.AddFirst(1);
+            doublyLinkedList.AddFirst(2);
+            doublyLinkedList.AddLast(3);
+            doublyLinkedList.AddLast(4);
+            doublyLinkedList.RemoveFirst();
+            doublyLinkedList.RemoveLast();
+
+            doublyLinkedList.ForEach(Console.WriteLine);
+            Console.WriteLine(string.Join(" ",doublyLinkedList.ToArray()));
+
+            doublyLinkedList.RemoveFirst();
+            doublyLinkedList.RemoveLast();
+        }
+    }
+}

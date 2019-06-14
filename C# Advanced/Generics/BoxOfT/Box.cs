78ece0184970bb29e9aec2c056cc02d568d79abb
BountyHunter <stormcrafterbg@gmail.com>
2019-06-14 14:13:32 +0300

    generic

---
+++ C# Advanced/Generics/BoxOfT/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> inner;
+        public Box()
+        {
+            inner = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return inner.Count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            inner.Push(element);
+        }
+
+        public T Remove()
+        {
+            return inner.Pop();
+        }
+    }
+}
---
+++ C# Advanced/Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    static public class ArrayCreator
+    { 
+        static public T[] Create<T>(int length, T item)
+        {
+            T[] newArr = new T[length];
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                newArr[i] = item;
+            }
+            return newArr;
+        }
+    }
+}

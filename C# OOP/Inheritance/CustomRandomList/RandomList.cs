4dbc7de16b6b016ac09cfb35e6214cfd60034dec
BountyHunter <stormcrafterbg@gmail.com>
2019-07-05 20:02:44 +0300

    lab

---
+++ C# OOP/Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = random.Next(0, Count);
+            string element = base[index];
+            Remove(element);
+            return element;
+        }
+    }
+}
---
+++ C# OOP/Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void AddRange(Stack<string> stack)
+        {
+            foreach (var str in stack)
+            {
+                Push(str);
+            }
+        }
+    }
+}

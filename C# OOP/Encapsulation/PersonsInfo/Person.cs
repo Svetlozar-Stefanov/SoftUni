3b7de1200a16a835df9bfabab4751b7c64cc2d36
BountyHunter <stormcrafterbg@gmail.com>
2019-07-08 15:46:00 +0300

    encapsulation

---
+++ C# OOP/Encapsulation/PersonsInfo/Person.cs
@@ -0,0 +1,84 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+
+        private string lastName;
+
+        private int age;
+
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                Validator.CheckFirstName(value);
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                Validator.CheckLastName(value);
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                Validator.CheckAge(value);
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                Validator.CheckSalary(value);
+                salary = value;
+            }
+        }
+
+        public decimal IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2m;
+            }
+            return Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/PersonsInfo/Validator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    static class Validator
+    {
+        public static void CheckFirstName(string value)
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+        }
+
+        public static void CheckLastName(string value)
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+        }
+
+        public static void CheckAge(int value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+        }
+
+        public static void CheckSalary(decimal value)
+        {
+            if (value < 460)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva!");
+            }
+        }
+    }
+}
